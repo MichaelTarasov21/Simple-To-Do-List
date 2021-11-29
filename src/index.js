62400316364bd8848709bf1233661dc47e3eb29f
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2021-11-29 13:04:58 -0500

    Changed environment variable name
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require("express");
 const path = require('path');
 const app = express();
-const port = process.env.DEV_PORT;
+const port = process.env.PORT;
 
 app.use(express.static(path.join(__dirname, "public")));
 
