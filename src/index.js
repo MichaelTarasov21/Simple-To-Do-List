386cc5a9e86e1dc0f49c875d1a9b26a855437f56
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2021-12-01 20:02:00 -0500

    Added default port for express webserver
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
-const path = require('path');
+const path = require("path");
+const internal = require("stream");
 const app = express();
-const port = process.env.PORT;
+let port = 8080;
+if (process.env.PORT) {
+	const port = process.env.PORT;
+}
 
 app.use(express.static(path.join(__dirname, "public")));
 
