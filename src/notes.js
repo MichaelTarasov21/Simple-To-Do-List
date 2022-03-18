53524455263e3dd6ef641a847a111c51f7f281f4
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2022-03-18 15:41:33 -0400

    Fixed typo in variable name
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- src/notes.js
+++ src/notes.js
@@ -1,14 +1,14 @@
 sendNotes = require("./sendnotes.js");
 addNote = require("./addNote.js");
-function notes(requset, res) {
+function notes(request, res) {
 	// A handler that determines what method is being called and hands off the request to the necessary function.
-	const method = requset.body.method;
+	const method = request.body.method;
 	switch (method) {
 		case "getnotes":
-			sendNotes(requset, res);
+			sendNotes(request, res);
 			break;
 		case "addnote":
-			addNote(requset, res);
+			addNote(request, res);
 			break;
 		default:
 			let response = {
