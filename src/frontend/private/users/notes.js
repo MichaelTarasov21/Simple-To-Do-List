dda5bb5ce2f4ec5f957750052de0448619265c76
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2022-03-26 10:41:31 -0400

    Added notes are now displayed
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- src/frontend/private/users/notes.js
+++ src/frontend/private/users/notes.js
@@ -1,21 +1,104 @@
+function clearnotepad() {
+	//Resets the note pad back to an empty state
+	const page = document.getElementById("page");
+	page.innerHTML = `
+		<span id="newnote"><button id="addnote" label="addnote">+</button> Add Note</span>
+	`;
+	document.getElementById("addnote").addEventListener("click", insertForm);
+}
 function getnotes() {
 	const xhttp = new XMLHttpRequest();
 
 	xhttp.open("POST", "/notes", true); // Uses post to ensure a broswer sends the request and does not merely assume that it already has the results (code 304)
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	xhttp.onreadystatechange = add_notes;
 	xhttp.send(`method=getnotes`);
 }
 function add_notes() {
+	function insertNote(note, index) {
+		// Insert a note dive then fill in the content. This is to prevent javascript injection into the webpage.
+		// The risk here is low since injecting the webpage requires one to have stored a note as the user in the past. However, the solution is not difficult to implement.
+		if (index === 0) {
+			// If the note is the first one. Insert some padding between notes and the new note form.
+			document.getElementById("page").insertAdjacentHTML(
+				"afterbegin",
+				`<br /><br />`
+			);
+		}
+		document.getElementById("page").insertAdjacentHTML("afterbegin", `<div class="note"></div>`);
+		const container = document.getElementsByClassName("note")[0];
+		container.innerText = note.message;
+		if (note.flag) {
+			container.innerText = `${note.flag} - ${container.innerText}`;
+		}
+	}
 	if (this.readyState == 4 && this.status == 200) {
 		const response = JSON.parse(this.responseText);
 		if (response.status === "Success") {
+			clearnotepad();
+			const notes = response.notes.reverse();
+			notes.forEach(insertNote);
+		} else {
+			alert("An error has occured. Please try again later.");
 		}
 	}
 }
 
-getnotes();
-
 function insertForm() {
+	function showDateInput() {
+		const expires = document.getElementById("expires").checked;
+		const dateInput = document.getElementById("dateInput");
+		if (expires) {
+			dateInput.style.visibility = "visible";
+		} else {
+			dateInput.style.visibility = "";
+		}
+	}
+
+	function sendForm() {
+		function reloadMessages() {
+			if (this.readyState == 4 && this.status == 200) {
+				const response = JSON.parse(this.responseText);
+				if (response.status === "Success") {
+					getnotes();
+				} else {
+					alert("An error has occured. Please try again later.");
+				}
+			}
+		}
+
+		const flag = document.getElementById("flag").value;
+		const message = document.getElementById("message").value;
+		const expires = document.getElementById("expires").checked;
+		let expireyDate = "";
+		if (expires) {
+			expireyDate = document.getElementById("expireyDate").value;
+			if (expireyDate === "") {
+				alert("If the note expires. Then please provide a date on which it should expire");
+				return;
+			}
+			const today = new Date();
+			const expiresOn = new Date(expireyDate);
+			const timeReamining = expiresOn - today;
+			if (timeReamining < 0) {
+				alert("It seems your note has already expired. Please double check the date.");
+				return;
+			}
+		}
+		if (message === "") {
+			alert("Message can not be blank");
+			return;
+		}
+
+		const xhttp = new XMLHttpRequest();
+		xhttp.open("POST", "/notes", true);
+		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhttp.onreadystatechange = reloadMessages;
+		xhttp.send(`method=addnote&flag=${flag}&message=${message}&expires=${expireyDate}`);
+
+		hideForm();
+	}
+
 	document.getElementById("newnote").innerHTML = `
 		<span class="note">
 			<datalist id="emotes">
@@ -43,58 +126,10 @@ function insertForm() {
 	document.getElementById("cancelNewNote").addEventListener("click", hideForm);
 	document.getElementById("addNewNote").addEventListener("click", sendForm);
 }
-function showDateInput() {
-	const expires = document.getElementById("expires").checked;
-	const dateInput = document.getElementById("dateInput");
-	if (expires) {
-		dateInput.style.visibility = "visible";
-	} else {
-		dateInput.style.visibility = "";
-	}
-}
+
 function hideForm() {
 	document.getElementById("newnote").innerHTML = `<button id="addnote" label="addnote">+</button> Add Note`;
 	document.getElementById("addnote").addEventListener("click", insertForm);
 }
-function sendForm() {
-	function reloadMessages() {
-		if (this.readyState == 4 && this.status == 200) {
-			//clear messages
-			//load messages
-			//yay
-		}
-	}
 
-	const flag = document.getElementById("flag").value;
-	const message = document.getElementById("message").value;
-	const expires = document.getElementById("expires").checked;
-	let expireyDate = "";
-	if (expires) {
-		expireyDate = document.getElementById("expireyDate").value;
-		if (expireyDate === "") {
-			alert("If the note expires. Then please provide a date on which it should expire");
-			return;
-		}
-		const today = new Date();
-		const expiresOn = new Date(expireyDate);
-		const timeReamining = expiresOn - today;
-		if (timeReamining < 0) {
-			alert("It seems your note has already expired. Please double check the date.");
-			return;
-		}
-	}
-	if (message === "") {
-		alert("Message can not be blank");
-		return;
-	}
-
-	const xhttp = new XMLHttpRequest();
-	xhttp.open("POST", "/notes", true);
-	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	xhttp.onreadystatechange = reloadMessages;
-	xhttp.send(`method=addnote&flag=${flag}&message=${message}&expires=${expireyDate}`);
-
-	hideForm();
-}
-
-document.getElementById("addnote").addEventListener("click", insertForm);
+getnotes();
