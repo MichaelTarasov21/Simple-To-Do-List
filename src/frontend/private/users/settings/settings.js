7c26a8b926533b9d8a53d847f7404f2dddf1b452
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2022-05-29 21:52:14 -0400

    Set up frontend to enable user deletion
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- src/frontend/private/users/settings/settings.js
+++ src/frontend/private/users/settings/settings.js
@@ -59,6 +59,30 @@ function setPassword() {
 	xhttp.send(`oldpassword=${oldpassword}&newpassword=${newpassword}`);
 }
 
+function deleteAccount() {
+	const password = document.getElementById("oldPassword").value;
+
+	const xhttp = new XMLHttpRequest();
+
+	xhttp.open("POST", "/users/delete", true);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.onreadystatechange = function () {
+		if (this.readyState == 4)
+			switch (this.status) {
+				case 400:
+					alert("Your current password is incorrect");
+					abortDeletion();
+					break;
+				case 500:
+					alert("An error has occured");
+				default:
+					window.location.reload(true); // Refresh the page to attempt recovery or complete deletion
+			}
+	};
+
+	xhttp.send(`password=${password}`);
+}
+
 function closeSettings() {
 	// Close the settings window and return to notes
 	window.location.pathname = window.location.pathname + "../";
@@ -81,9 +105,44 @@ function parseSettings() {
 	}
 }
 
+function confirmDeletion() {
+	const oldpassword = document.getElementById("oldPassword").value;
+
+	if (oldpassword === "") {
+		alert("Please enter your password in order to delete your account");
+		return;
+	} else {
+		document.getElementById("confirmation").style.display = "block";
+	}
+}
+
 document.getElementById("logout").addEventListener("click", logout);
 document.getElementById("close").addEventListener("click", closeSettings);
 document.getElementById("update").addEventListener("click", parseSettings);
+document.getElementById("delete").addEventListener("click", confirmDeletion);
+
+// Clear confirmation on page load
+document.getElementById("acknowledgement").value = "";
+
+function checkConfirmation() {
+	// Check if the text inside of the acknowledgement box is a predefined value
+	// Unlock the confirmation button only if text matches preset value
+	const text = document.getElementById("acknowledgement").value;
+
+	if (text === "Delete My Account") {
+		document.getElementById("confirm").style.visibility = "visible";
+	} else {
+		document.getElementById("confirm").style.visibility = "hidden";
+	}
+}
+
+function abortDeletion() {
+	document.getElementById("confirmation").style.display = "";
+}
+
+document.getElementById("acknowledgement").addEventListener("input", checkConfirmation);
+document.getElementById("cancelDeletion").addEventListener("click", abortDeletion);
+document.getElementById("confirm").addEventListener("click", deleteAccount);
 
 function getEmail() {
 	// Get the email value already on file for this account
