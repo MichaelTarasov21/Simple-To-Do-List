bb5dd124269094277deaabe49dda9ce6f31a10b2
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2022-09-29 23:00:29 -0400

    Expiring notes now designated as such in the notepad
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- src/frontend/private/users/notes/notes.js
+++ src/frontend/private/users/notes/notes.js
@@ -59,24 +59,48 @@ function deleteNote(event) {
 }
 
 function add_notes() {
-	function insertNote(note, index) {
+	let total_notes;
+	const date = new Date();
+	const today = date.toJSON().slice(0, 10);
+	const expiring_soon = [];
+	const regular = [];
+	function insertNote(note, mark = false) {
 		// Insert a note div then fill in the content. This is to prevent javascript injection into the webpage.
 		// The risk here is low since injecting the webpage requires one to have stored a note as the user in the past. However, the solution is not difficult to implement.
-		if (index === 0) {
-			// If the note is the last one. Insert some padding between notes and the new note form.
-			document.getElementById("notes").insertAdjacentHTML("afterbegin", `<br /><br />`);
-		}
 		document.getElementById("notes").insertAdjacentHTML("afterbegin", `<div class="note" noteID="${note.noteid}"></div>`);
 		const container = document.getElementsByClassName("note")[0];
 		container.innerText = note.message;
 		if (note.flag) {
 			container.innerText = `${note.flag} - ${container.innerText}`;
 		}
+		if (mark) {
+			container.innerText = `🕚 - ${container.innerText}`;
+			container.title = "Expiring soon"
+			container.classList.add("expiring");
+		}
 		if (note.completed) {
 			container.classList.add("completed");
+			container.classList.remove("expiring");
 			container.addEventListener("click", uncompleteNote);
 		} else {
 			container.addEventListener("click", completeNote);
+		}
+	}
+	function sortNote(note, index) {
+		console.log(note.expiration_date);
+		if (note.expiration_date) {
+			if (note.expiration_date.slice(0, 10) === today) {
+				expiring_soon.push(note);
+			}
+		} else {
+			regular.push(note);
+		}
+
+		if (index + 1 === total_notes) {
+			// Insert some padding between the notes and the new note form.
+			document.getElementById("notes").insertAdjacentHTML("afterbegin", `<br /><br />`);
+			regular.forEach(function(note){insertNote(note)});
+			expiring_soon.forEach(function(note){insertNote(note, true)});
 		}
 	}
 	if (this.readyState == 4 && this.status == 200) {
@@ -84,7 +108,8 @@ function add_notes() {
 		if (response.status === "Success") {
 			clearnotepad();
 			const notes = response.notes.reverse();
-			notes.forEach(insertNote);
+			total_notes = notes.length;
+			notes.forEach(sortNote);
 		} else {
 			alert("An error has occured. Please try again later.");
 		}
