b7ad5769607486e4bdc5f73db4de2cf1d3594415
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2022-09-29 21:40:30 -0400

    Expired notes no longer appear on page
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- src/notes/sendnotes.js
+++ src/notes/sendnotes.js
@@ -30,7 +30,7 @@ function sendnotes(request, res) {
 	const date = new Date();
 	const today = mysql.escape(date.toJSON().slice(0, 10));
 
-	sql.query(`SELECT * FROM tasks WHERE (userid=${userid} AND (completed=0 OR completed_date = ${today}))`, function (err, result) {
+	sql.query(`SELECT * FROM tasks WHERE (userid=${userid} AND (completed=0 OR completed_date = ${today}) AND (expiration_date >= ${today} OR expiration_date IS NULL))`, function (err, result) {
 		if (err) {
 			res.send(response);
 			sql.end();
