e000068cf77ac799456f7c872aeb415569fdca03
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2022-10-28 22:38:46 -0400

    Refactored form sending into seperate file
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- src/frontend/private/users/notes/insertForm.js
+++ src/frontend/private/users/notes/insertForm.js
@@ -1,6 +1,6 @@
 import GraphemeSplitter from "./grapheme-splitter.js";
 import { hideForm } from "./hideForm.js";
-import { reloadMessages } from "./reloadMessages.js";
+import { sendForm } from "./sendForm.js";
 
 export function insertForm() {
 	function lengthLimit() {
@@ -34,38 +34,7 @@ export function insertForm() {
 			repetition_input.style.visibility = "";
 		}
 	}
-	function sendForm() {
-		const flag = document.getElementById("flag").value;
-		const message = document.getElementById("message").value;
-		const expires = document.getElementById("expires").checked;
-		let expireyDate = "";
-		if (expires) {
-			expireyDate = document.getElementById("expireyDate").value;
-			if (expireyDate === "") {
-				alert("If the note expires. Then please provide a date on which it should expire");
-				return;
-			}
-			const today = new Date();
-			const expiresOn = new Date(expireyDate);
-			const timeReamining = expiresOn - today;
-			if (timeReamining < 0) {
-				alert("It seems your note has already expired. Please double check the date.");
-				return;
-			}
-		}
-		if (message === "") {
-			alert("Message can not be blank");
-			return;
-		}
 
-		const xhttp = new XMLHttpRequest();
-		xhttp.open("POST", "/notes", true);
-		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xhttp.onreadystatechange = reloadMessages;
-		xhttp.send(`method=addnote&flag=${flag}&message=${message}&expires=${expireyDate}`);
-
-		hideForm();
-	}
 	document.getElementById("newnote").innerHTML = `
 		<span class="note hoverDisable">
 			<datalist id="emotes">
@@ -110,4 +79,3 @@ export function insertForm() {
 	document.getElementById("cancelNewNote").addEventListener("click", hideForm);
 	document.getElementById("addNewNote").addEventListener("click", sendForm);
 }
-
---
+++ src/frontend/private/users/notes/sendForm.js
@@ -0,0 +1,35 @@
+import { hideForm } from "./hideForm.js";
+import { reloadMessages } from "./reloadMessages.js";
+
+export function sendForm() {
+	const flag = document.getElementById("flag").value;
+	const message = document.getElementById("message").value;
+	const expires = document.getElementById("expires").checked;
+	let expireyDate = "";
+	if (expires) {
+		expireyDate = document.getElementById("expireyDate").value;
+		if (expireyDate === "") {
+			alert("If the note expires. Then please provide a date on which it should expire");
+			return;
+		}
+		const today = new Date();
+		const expiresOn = new Date(expireyDate);
+		const timeReamining = expiresOn - today;
+		if (timeReamining < 0) {
+			alert("It seems your note has already expired. Please double check the date.");
+			return;
+		}
+	}
+	if (message === "") {
+		alert("Message can not be blank");
+		return;
+	}
+
+	const xhttp = new XMLHttpRequest();
+	xhttp.open("POST", "/notes", true);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.onreadystatechange = reloadMessages;
+	xhttp.send(`method=addnote&flag=${flag}&message=${message}&expires=${expireyDate}`);
+
+	hideForm();
+}
