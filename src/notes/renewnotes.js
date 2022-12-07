dd212d87212aad7e69b9ebd74f67c6d58d6f3d2f
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2022-12-07 14:03:10 -0500

    Renewal now works for expiring notes
    The script now updates the expiration date for expiring notes.
    
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- src/notes/renewnotes.js
+++ src/notes/renewnotes.js
@@ -48,15 +48,27 @@ function renewNotes() {
 				}
 				break;
 		}
-		sql.query(`UPDATE tasks SET posted_date = ${mysql.escape(renewal.toJSON().slice(0, 10))}, completed = 0, completed_date = NULL WHERE (noteid = ${note.noteid})`, function(err) {
+		sql.query(`UPDATE tasks SET posted_date = ${mysql.escape(renewal.toJSON().slice(0, 10))}, completed = 0, completed_date = NULL WHERE (noteid = ${note.noteid})`, function (err) {
 			if (err) {
 				console.error("error in SQL Query: " + err.stack);
 				return;
 			}
 		});
+		if (note.expiration_date) {
+			// Extend the expiration date for notes that expire
+			const expires = new Date(note.expiration_date);
+			const last_post_date = new Date(note.posted_date);
+			expires.setMilliseconds(renewal - last_post_date);
+			sql.query(`UPDATE tasks SET expiration_date = ${mysql.escape(expires.toJSON().slice(0, 10))} WHERE (noteid = ${note.noteid})`, function (err) {
+				if (err) {
+					console.error("error in SQL Query: " + err.stack);
+					return;
+				}
+			});
+		}
 	}
 
-	sql.query(`SELECT noteid, posted_date, repeats FROM tasks WHERE (completed = 1 AND repeats != 0 AND completed_date != ${mysql.escape(today.toJSON().slice(0, 10))})`, function (err, result) {
+	sql.query(`SELECT noteid, posted_date, expiration_date, repeats FROM tasks WHERE (completed = 1 AND repeats != 0 AND completed_date != ${mysql.escape(today.toJSON().slice(0, 10))})`, function (err, result) {
 		if (err) {
 			console.error("error in SQL Query: " + err.stack);
 			return;
