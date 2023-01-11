d7cd02bfb57e20a1bc54c88ba6d89c0c3ebc293f
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2023-01-11 18:53:01 -0500

    Created path for hosting admin panel assets
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- src/administration/adminRoutes.js
+++ src/administration/adminRoutes.js
@@ -5,6 +5,6 @@ const path = require("path");
 router.use('/', express.static(path.join(__dirname, "../frontend/private/administration")))
 router.use('/styles/', express.static(path.join(__dirname, "../frontend/private/administration/StyleSheets/CSS")))
 router.use('/scripts/', express.static(path.join(__dirname, "../frontend/private/administration/Scripts")))
-
+router.use('/assets/', express.static(path.join(__dirname, "../frontend/private/administration/Assets")))
 
 module.exports = router;
