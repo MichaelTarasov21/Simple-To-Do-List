e4d6cc7b4e1ee08f0a56209079092e6441bb723d
Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>
2023-01-11 19:01:11 -0500

    Send admin panel file instead of providing directory access
    Signed-off-by: Michael Tarasov <71594701+MichaelTarasov21@users.noreply.github.com>

--- src/administration/adminRoutes.js
+++ src/administration/adminRoutes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-router.use('/', express.static(path.join(__dirname, "../frontend/private/administration")))
-router.use('/styles/', express.static(path.join(__dirname, "../frontend/private/administration/StyleSheets/CSS")))
-router.use('/scripts/', express.static(path.join(__dirname, "../frontend/private/administration/Scripts")))
-router.use('/assets/', express.static(path.join(__dirname, "../frontend/private/administration/Assets")))
+router.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname, "../frontend/private/administration/admin.html"));
+});
+router.use("/styles/", express.static(path.join(__dirname, "../frontend/private/administration/StyleSheets/CSS")));
+router.use("/scripts/", express.static(path.join(__dirname, "../frontend/private/administration/Scripts")));
+router.use("/assets/", express.static(path.join(__dirname, "../frontend/private/administration/Assets")));
 
 module.exports = router;
